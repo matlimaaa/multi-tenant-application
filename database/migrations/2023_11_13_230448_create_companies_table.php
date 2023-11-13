4843d95421a2e3226550f0f749cbb4905055e894
Mateus Lima de Freitas <dev.org.ml@gmail.com>
2023-11-13 20:30:42 -0300

    feat: create company model and its migration

---
+++ database/migrations/2023_11_13_230448_create_companies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('domain')->unique();
+            $table->string('bd_database')->unique();
+            $table->string('bd_hostname');
+            $table->string('bd_username');
+            $table->string('bd_password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
