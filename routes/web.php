ea5b39bbcfd64fa063c6d594647834514b4bc434
Mateus Lima de Freitas <dev.org.ml@gmail.com>
2023-11-14 20:53:49 -0300

    fix: adjust route 404

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('404', function () {
-    return response()->view('errors.404-tenant', [], 404);
+    return view('errors.404-tenant');
 })->name('404.tenant');
 
 Route::get('/', function () {
