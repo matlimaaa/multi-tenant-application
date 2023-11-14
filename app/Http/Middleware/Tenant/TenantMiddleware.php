c0adc27924a53e3e4c39eedf209c674a17f8c586
Mateus Lima de Freitas <dev.org.ml@gmail.com>
2023-11-14 20:53:30 -0300

    refactor: adjust redirection to 404 page and help middleware to apply the connection to the bank through the tenant

--- app/Http/Middleware/Tenant/TenantMiddleware.php
+++ app/Http/Middleware/Tenant/TenantMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware\Tenant;
 
 use App\Models\Company;
+use App\Tenant\ManagerTenant;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,8 +19,10 @@ class TenantMiddleware
     {
         $company = $this->getCompanyByDomain($request->getHost());
 
-        if (! $company) {
-            return response()->view('errors.404-tenant', [], 404);
+        if (! $company && $request->url() != route('404.tenant')) {
+            return redirect()->route('404.tenant');
+        } else if ($request->url() != route('404.tenant')){
+            app(ManagerTenant::class)->setConnection($company);
         }
 
         return $next($request);
