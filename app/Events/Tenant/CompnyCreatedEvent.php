7e5e562e7b228cc875e4a6a867d1f5fc5a431f77
Mateus Lima de Freitas <dev.org.ml@gmail.com>
2023-11-19 10:53:07 -0300

    feat: create a new event and its listener to create the company database automatically

---
+++ app/Events/Tenant/CompnyCreatedEvent.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Events\Tenant;
+
+use App\Models\Company;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class CompnyCreatedEvent
+{
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(public Company $company)
+    {
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('channel-name'),
+        ];
+    }
+}
