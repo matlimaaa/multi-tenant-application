aa3a06b45e6fa8321049ffe04ffb5864e3b2d201
Mateus Lima de Freitas <dev.org.ml@gmail.com>
2023-11-20 09:39:32 -0300

    feat: create CRUD and routes for enterprises model

--- app/Http/Controllers/Tenant/CompanyController.php
+++ app/Http/Controllers/Tenant/CompanyController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Tenant;
 
 use App\Events\Tenant\CompnyCreatedEvent;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateCompanyRequest;
+use App\Http\Resources\CompanyResource;
 use App\Models\Company;
+use Illuminate\Http\Response;
 
 class CompanyController extends Controller
 {
@@ -12,17 +15,56 @@ class CompanyController extends Controller
     {
     }
 
-    public function store()
+    public function index()
+    {
+        return response()->json([
+            'companies' => CompanyResource::collection($this->company->all()),
+        ]);
+    }
+
+    public function store(StoreUpdateCompanyRequest $request)
     {
         $company = $this->company->create([
-            'name' => 'test',
-            'domain' => 'test.local' . random_int(0, PHP_INT_MAX),
-            'bd_database' => 'banco_' . random_int(0, PHP_INT_MAX),
-            'bd_hostname' => 'mysql',
-            'bd_username' => 'root',
-            'bd_password' => 'root',
+            'name' => $request->name,
+            'domain' => $request->domain,
+            'bd_database' => $request->bd_database,
+            'bd_hostname' => $request->bd_hostname,
+            'bd_username' => $request->bd_username,
+            'bd_password' => $request->bd_password,
         ]);
 
         event(new CompnyCreatedEvent($company));
+    }
+
+    public function show(string $domain)
+    {
+        $company = $this->company->where('domain', $domain)->firstOrFail();
+
+        return response()->json(new CompanyResource($company));
+    }
+
+    public function update(StoreUpdateCompanyRequest $request, string $domain)
+    {
+        $company = $this->company->where('domain', $domain)->firstOrFail();
+
+        $company->update([
+            'name' => $request->name,
+            'domain' => $request->domain,
+            'bd_database' => $request->bd_database,
+            'bd_hostname' => $request->bd_hostname,
+            'bd_username' => $request->bd_username,
+            'bd_password' => $request->bd_password,
+        ]);
+
+        return response()->json(['message' => 'updated']);
+    }
+
+    public function destroy(string $domain)
+    {
+        $company = $this->company->where('domain', $domain)->firstOrFail();
+
+        $company->delete();
+
+        return response()->json([], Response::HTTP_NO_CONTENT);
     }
 }
--- routes/tenant.php
+++ routes/tenant.php
@@ -3,8 +3,11 @@
 use App\Http\Controllers\Tenant\CompanyController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return 'ola';
+Route::prefix('company')->name('tenants.company')->group(function () {
+    Route::get('/', [CompanyController::class, 'index'])->name('index');
+    Route::post('/', [CompanyController::class, 'store'])->name('store');
+    Route::get('/{domain}', [CompanyController::class, 'show'])->name('show');
+    Route::put('/{domain}', [CompanyController::class, 'update'])->name('update');
+    Route::delete('/{domain}', [CompanyController::class, 'destroy'])->name('destroy');
 });
 
-Route::get('company/store', [CompanyController::class, 'store'])->name('tenant.store');
